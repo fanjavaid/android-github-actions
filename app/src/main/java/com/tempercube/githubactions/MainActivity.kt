d5f59d44f2dc78aad7a988ec871957fcaff5af4c
Fandi Akhmad <fanjavaid@gmail.com>
2021-09-08 09:27:42 +0700

    Mimic error

--- app/src/main/java/com/tempercube/githubactions/MainActivity.kt
+++ app/src/main/java/com/tempercube/githubactions/MainActivity.kt
@@ -3,6 +3,7 @@ package com.tempercube.githubactions
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
