295fab64b5ccdd291cbcdc5fab3c18aefb0d3b05
Fandi Akhmad <fanjavaid@gmail.com>
2021-09-09 09:07:54 +0700

    Fix linter

--- app/src/main/java/com/tempercube/githubactions/MainActivity.kt
+++ app/src/main/java/com/tempercube/githubactions/MainActivity.kt
@@ -2,7 +2,6 @@ package com.tempercube.githubactions
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
